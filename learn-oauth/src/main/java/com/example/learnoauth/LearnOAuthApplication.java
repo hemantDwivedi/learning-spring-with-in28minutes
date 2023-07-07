e396aae369c071e763b088ef1f004d27ab830e51
hemantDwivedi <hemantdwiwedi@gmail.com>
2023-07-07 20:33:09 +0530

    Learnt OAuth Security

---
+++ learn-oauth/src/main/java/com/example/learnoauth/LearnOAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnOAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnOAuthApplication.class, args);
+	}
+
+}
