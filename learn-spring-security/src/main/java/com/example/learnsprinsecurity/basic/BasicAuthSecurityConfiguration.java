35e9419d1299891b1692d92ecdbb436cc86e56dc
hemantDwivedi <hemantdwiwedi@gmail.com>
2023-07-07 12:46:34 +0530

    Bcrypt encoded passwords

--- learn-spring-security/src/main/java/com/example/learnsprinsecurity/basic/BasicAuthSecurityConfiguration.java
+++ learn-spring-security/src/main/java/com/example/learnsprinsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -2,15 +2,23 @@ package com.example.learnsprinsecurity.basic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class BasicAuthSecurityConfiguration {
@@ -21,22 +29,58 @@ public class BasicAuthSecurityConfiguration {
         httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         httpSecurity.httpBasic(Customizer.withDefaults());
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        httpSecurity.headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
         return httpSecurity.build();
     }
 
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        var user = User.withUsername("hemant")
+//                .password("{noop}hemant")
+//                .roles(String.valueOf(roles.USER))
+//                .build();
+//
+//        var admin = User.withUsername("admin")
+//                .password("{noop}admin")
+//                .roles(String.valueOf(roles.ADMIN))
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
+
     @Bean
-    public UserDetailsService userDetailsService() {
+    public UserDetailsService userDetailsService(DataSource dataSource) {
         var user = User.withUsername("hemant")
-                .password("{noop}hemant")
+//                .password("{noop}hemant")
+                .password("hemant")
+                .passwordEncoder(pwd -> passwordEncoder().encode(pwd))
                 .roles(String.valueOf(roles.USER))
                 .build();
 
         var admin = User.withUsername("admin")
-                .password("{noop}admin")
+                .password("admin")
+                .passwordEncoder(pwd -> passwordEncoder().encode(pwd))
                 .roles(String.valueOf(roles.ADMIN))
                 .build();
 
-        return new InMemoryUserDetailsManager(user, admin);
+        var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(admin);
+        jdbcUserDetailsManager.createUser(user);
+
+        return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
     private enum roles {
