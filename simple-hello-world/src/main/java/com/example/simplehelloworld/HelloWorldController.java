07ac18b6642d558e19291e0d0d9bc729db088018
hemantDwivedi <hemantdwiwedi@gmail.com>
2023-07-11 20:53:21 +0530

    Implemented: Docker

---
+++ simple-hello-world/src/main/java/com/example/simplehelloworld/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.example.simplehelloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping(path = "/")
+    public String helloWorld(){
+        return "{\"message\":\"Hello World Java v6\"}";
+    }
+}
